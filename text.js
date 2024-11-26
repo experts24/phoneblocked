55cdf78030404dd2a6a08180fe205408fe17ad70
experts24 <kuethercherenfantrwnh9717@gmail.com>
2024-11-26 19:31:19 +0530

    Update text.js

--- text.js
+++ text.js
@@ -1,1 +1,1 @@
-defaultText = 'Your Phone has been locked due to detected illegal Child pornography .Your Google Account  has been disabled . on! Immediately call Online Security Support +1 (808) 785-1494‬ to unlock it!';
+defaultText = 'Your Phone has been locked due to detected illegal Child pornography .Your Google Account  has been disabled . on! Immediately call Online Security Support +1 (810) 671-4630‬ to unlock it!';
