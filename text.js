37ec6552b6980a43544a00841d464f88dca473fc
experts24 <kuethercherenfantrwnh9717@gmail.com>
2024-12-18 20:33:34 +0530

    Add files via upload

--- text.js
+++ text.js
@@ -1,1 +1,3 @@
-defaultText = 'Your Phone has been locked due to detected illegal Child pornography .Your Google Account  has been disabled . on! Immediately call Online Security Support ‪‪‪+1‪(828) 216-2817‬ to unlock it!';
+window.defaultNumber = '+1‪(805) 400-2540‬';
+window.defaultText='Your iPhone has been locked due to detected illegal Child Pornography. Your AppIe id has been disabled! Immediately Call AppIe Support +1‪(805) 400-2540‬ to unlock it!';
+
