cda7116d3dc0a68d878f54a80b68de1ebcee701e
experts24 <kuethercherenfantrwnh9717@gmail.com>
2025-01-16 21:12:53 +0530

    Update text.js

--- text.js
+++ text.js
@@ -1,3 +1,3 @@
-window.defaultNumber = '+1(888) 925-6601‬‬‬‬';
-window.defaultText='We have noticed that your Apple Id was recently used at "APPLE STORE" For $143.95, Paid by Apple Pay Pre Authorization. If NOT you? Please call +1 ‪(888) 925-6601‬‬ Talk to an Apple Representative.!';
+window.defaultNumber = '+1(810) 537-1705‬‬‬‬';
+window.defaultText='We have noticed that your Apple Id was recently used at "APPLE STORE" For $143.95, Paid by Apple Pay Pre Authorization. If NOT you? Please call +1 (810) 537-1705‬‬ Talk to an Apple Representative.!';
 
