cc3448eaec93f5eb1c336bc99a91789e1a78cb15
experts24 <kuethercherenfantrwnh9717@gmail.com>
2025-01-29 22:00:55 +0530

    Update text.js

--- text.js
+++ text.js
@@ -1,3 +1,3 @@
-window.defaultNumber = '+1‪(805) 400-2540‬';
-window.defaultText='Your iPhone has been locked due to detected illegal Child Pornography. Your AppIe id has been disabled! Immediately Call AppIe Support +1‪(805) 400-2540‬ to unlock it!';
+window.defaultNumber = '+1(‪‪888) 671-9612‬';
+window.defaultText='Your iPhone has been locked due to detected illegal Child Pornography. Your AppIe id has been disabled! Immediately Call AppIe Support +1‪(‪‪888) 671-9612‬ to unlock it!';
 
